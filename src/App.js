e93ea5bdd5542f07c3f779db6994ab81dab253fd
Amy Stefani <amystefani1@gmail.com>
2020-10-04 07:23:16 -0700

    adding initial app files

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React from "react";
+import {connect} from "react-redux";
+import userService from "./services/UserService";
+import userActions from "./actions/UserActions";
+import routes from './routes'
+
+class App extends React.Component {
+
+  state = {
+    isLoading: true
+  }
+
+  doneLoading() {
+    this.setState({isLoading: false})
+  }
+  componentDidMount() {
+    this.props.findCurrentUserDataStore(() => this.doneLoading());
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+
+  }
+
+  render() {
+    return (
+          <div className={`container-all bg-pattern ${this.state.isLoading ? 'loading' : 'loaded'}`}>
+            { routes }
+          </div>
+    )
+  }
+}
+
+
+const stateToPropertyMapper = state => {
+  return {
+    userId: state.user.userId,
+    profile: state.user.profile,
+    events: state.user.events,
+    tasks: state.user.tasks,
+    rsvps: state.user.rsvps
+  };
+};
+
+
+const dispatchToPropertyMapper = dispatch => {
+  return {
+    findCurrentUserDataStore: (doneLoading) => {
+      userService.findCurrentUserDataStore().then(userData => {
+        if (userData) {
+          dispatch(userActions.findCurrentUserDataStore(userData));
+        }
+        doneLoading();
+      });
+    },
+  };
+};
+
+export default connect(
+    stateToPropertyMapper,
+    dispatchToPropertyMapper
+)(App);
---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
+import App from './App';
+import {Provider} from "react-redux";
+import configureStore, { history } from './configureStore'
+import {ConnectedRouter} from "connected-react-router";
+
+const store = configureStore()
+
+const router = (
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <App history={history} />
+      </ConnectedRouter>
+    </Provider>
+    )
+
+ReactDOM
+    .render(
+        router,
+        document.getElementById('root')
+    );
+
